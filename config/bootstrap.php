b2aa39cb4d1e46bbffdee409dcfe6279a123766a
Brad Van Skyhawk <brad@vanskyhawk.com>
2019-08-19 20:54:20 -0600

    deprecations

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -4,9 +4,9 @@ use Cake\Event\Event;
 use Cake\Network\Request;
 
 EventManager::instance()->on('Controller.initialize', function (Event $event) {
-    $controller = $event->subject();
+    $controller = $event->getSubject();
     if ($controller->components()->has('RequestHandler')) {
-        $controller->RequestHandler->config('viewClassMap.xlsx', 'CakeExcel.Excel');
+        $controller->RequestHandler->setConfig('viewClassMap.xlsx', 'CakeExcel.Excel');
     }
 });
 
